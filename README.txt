2770041cdb35c8ea66e592857cc8726c0dc9330a
Arin Santry <arinsantry@gmail.com>
2021-12-27 14:26:34 -0500

    Added README.txt, added comments

---
+++ README.txt
@@ -0,0 +1,7 @@
+Author: Arin Santry
+Class: Data Structures and Algorithms, Fall 2019
+Built in: Visual Studio
+
+The purpose of this program is to check if a given set of punctuation (such as parenthases or brackets) are in pairs.
+The purpose of this assignment was to practice using stacks.
+The strings to be checked are read from testme.txt, and the results are written in results.txt
--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,7 @@
 #include <stack>
 using namespace std;
 
+// check that punctuation are in correct pairs
 bool ArePair(char opening, char closing)
 {
 	if (opening == '(' && closing == ')')
@@ -18,21 +19,28 @@ bool ArePair(char opening, char closing)
 		return false;
 }
 
+// STACKS!
 bool AreBalanced(string exp)
 {
 	stack<char> parStack;
 	for (int i = 0; i < exp.length(); i++)
 	{
+		// if there is an opening punctuation, push it onto the stack
 		if (exp[i] == '(' || exp[i] == '[' || exp[i] == '{' || exp[i] == '<')
 			parStack.push(exp[i]);
+		// if there is a closing punctuation...
 		else if (exp[i] == ')' || exp[i] == ']' || exp[i] == '}' || exp[i] == '>')
 		{
+			// if the stack is empty or the top of the stack is not a pair with the character, 
+			// then the string is not balanced
 			if (parStack.empty() || !ArePair(parStack.top(), exp[i]))
 				return false;
-			else
+			else // otherwise, pop the top of the stack
 				parStack.pop();
 		}
 	}
+	// if the stack is empty by the end of this function, then the string is balanced
+	// if not, the string is not balanced
 	return parStack.empty() ? true : false;
 }
 
@@ -40,8 +48,8 @@ int main()
 {
 	ifstream inStream;
 	ofstream outStream;
-	inStream.open("testme.txt");
-	outStream.open("results.txt");
+	inStream.open("testme.txt"); // test input file
+	outStream.open("results.txt"); // where the results will be stored
 	string expression;
 
 	if (inStream.fail())
@@ -65,6 +73,7 @@ int main()
 		cout << '\n' << "Press a key to continue...";
 	} while (cin.get() != '\n');
 
+	// close the streams!!
 	inStream.close();
 	outStream.close();
 	return 0;
